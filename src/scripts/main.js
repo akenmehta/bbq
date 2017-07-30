a93abf491726aa00f5886ceea0f4399fccff7517
Aken Mehta <mehta.aken@gmail.com>
2017-07-30 18:21:16 -0400

    added browserify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,24 @@
 var model = {
-	init: function() {
-		console.log('Model initialized');
-	}
+  init: function() {
+    console.log('Model initialized');
+  }
 };
 
-var helper = {};
+var helper = {
+
+};
 
 var view = {
-	init: function() {
-		console.log('View initialized');
-	}
+  init: function() {
+    console.log('View initialized');
+  }
 };
 
 var app = {
-	init: function() {
-		model.init();
-		view.init();
-	}
+  init: function() {
+    model.init();
+    view.init();
+  }
 };
 
 app.init();
