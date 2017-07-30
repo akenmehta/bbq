9801f6b471e7ea9ed1ba603f055208159b993491
Aken Mehta <mehta.aken@gmail.com>
2017-07-30 19:13:08 -0400

    added menu functionality

--- src/scripts/modules/helper.js
+++ src/scripts/modules/helper.js
@@ -4,5 +4,21 @@ var model = require('./model.js');
 var helper = module.exports = {
   dateDiff: function() {
     return countdown(model.currentDate, model.eventDate);
+  },
+
+  setCurrent: function(id) {
+  	for(var i = 0; i < model.menu.length; i++) {
+  		if(model.menu[i].id === id) {
+  			model.menuItem = model.menu[i]		
+  		}
+  	}
+  },
+
+  increment: function() {
+  	model.menuItem.count++;
+  },
+
+  getCurrent: function() {
+  	return model.menuItem;
   }
 };
--- src/scripts/modules/view.js
+++ src/scripts/modules/view.js
@@ -3,6 +3,21 @@ var helper = require('./helper.js');
 var view = module.exports = {
   init: function() {
     view.showCountdown();
+    view.registerClick();
+  },
+
+  registerClick: function() {
+  	var id;
+  	var listItem = document.querySelectorAll('#menu li');
+
+  	for(var i = 0; i < listItem.length; i++) {
+  		listItem[i].addEventListener('click', function() {
+  			id = this.getAttribute('data-id');
+  			helper.setCurrent(+id);
+  			helper.increment();
+  			view.updateCount();
+  		});
+  	}
   },
 
   showCountdown: function() {
@@ -11,5 +26,12 @@ var view = module.exports = {
     document.querySelector('#years').innerHTML = diff.years;
     document.querySelector('#months').innerHTML = diff.months;
     document.querySelector('#days').innerHTML = diff.days;
+  },
+
+  updateCount: function() {
+  	var item = helper.getCurrent();
+  	var li = document.querySelector('[data-id="' + item.id + '"]');
+  	var count = li.querySelector('.menu-count');
+  	count.innerHTML = item.count;
   }
 };
